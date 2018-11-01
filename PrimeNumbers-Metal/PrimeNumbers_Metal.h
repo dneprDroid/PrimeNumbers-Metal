cc81914667c60ea99fc2aeae7b5f807407334f52
Alex iOS <alex.ovechko@softensy.com>
2018-11-01 21:33:01 +0200

    Fix iOS module reps.

---
+++ PrimeNumbers-Metal/PrimeNumbers_Metal.h
@@ -0,0 +1,19 @@
+//
+//  PrimeNumbers_Metal.h
+//  PrimeNumbers-Metal
+//
+//  Created by user on 11/1/18.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PrimeNumbers_Metal.
+FOUNDATION_EXPORT double PrimeNumbers_MetalVersionNumber;
+
+//! Project version string for PrimeNumbers_Metal.
+FOUNDATION_EXPORT const unsigned char PrimeNumbers_MetalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrimeNumbers_Metal/PublicHeader.h>
+
+
