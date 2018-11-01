3442fb7ab07e7defd643013ab9c3192ce4262ad6
Alex iOS <alex.ovechko@softensy.com>
2018-11-01 13:54:06 +0200

    Remove unused headers.

--- PrimeNumbers-Metal/PrimeNumbers_Metal.h
+++
@@ -1,9 +0,0 @@
-//
-//  PrimeNumbers_Metal.h
-//  PrimeNumbers-Metal
-//
-//  Created by user on 10/31/18.
-//  Copyright © 2018 Alex. All rights reserved.
-//
-
-#import "PrimeNumbers_Metal-Swift.h"
